719523fbe0049b354aa682c36d7c302f90714894
John Wheeler <johnwheeler@Johns-MBP.lan>
2024-06-18 19:13:16 -0400

    chore(animations): add animations, finish out the end of the 5 hours by adding TODO commentsabout what my next steps would be

--- src/app/components/site-nav/index.tsx
+++ src/app/components/site-nav/index.tsx
@@ -3,6 +3,10 @@ import Link from "next/link";
 import styles from "./index.module.scss";
 
 export default function SiteNav() {
+  //TODO: build out nav dropdowns, potentially make each dropdown an individual component and import each
+  //so that they can be programatically made responsive for mobile
+  //TODO: add hover states for the links to change to orange color on hover, and to button to change to purple-ish bg on hover
+
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
